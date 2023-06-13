ccc138019b6ca515214cf7003cae01cac6b3a252
Lattapan <noey.rachata@gmail.com>
2023-06-13 20:27:56 +0700

    Add content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import "../App.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <main className="about-page-wrapper">
+      <h1>Let's Get to Know What This Page is About!</h1>
+
+      <h2>What it is</h2>
+      <p>
+        Welcome to this proundly designed, responsive website! This is a beauty
+        lover's dream come true! Here, you'll discover an incredible array of
+        makeup products sourced from the Makeup API. This site boasts a sleek
+        and modern design, making it a delight to explore makeup items for your
+        face, eye, and lips.
+      </p>
+
+      <h2>What it offers</h2>
+      <p>
+        Every product showcased here acts as a gateway to its brand's page,
+        treating you to detailed information, helpful customer reviews, and the
+        convenience of making purchases. While the newsletter and social media
+        sections may be static, they bring an authentic blog-like feel to the
+        experience, making you feel right at home.
+      </p>
+
+      <h2>Why this site exists</h2>
+      <p>
+        Not only does this project offer a chance for me to sharpen my React
+        skills, but it also allows me to dive into React Router implementation
+        and API integration. I'm thrilled to have you join me on this exciting
+        journey, where technology and beauty harmoniously blend together!
+      </p>
+
+      <h3>Disclaimer:</h3>
+      <p>
+        When you click on a product to make a purchase, you might notice that
+        some of the product links have been taken down by the owner. It could be
+        because they no longer sell those products.
+      </p>
+    </main>
+  );
 };
 
 export default About;
